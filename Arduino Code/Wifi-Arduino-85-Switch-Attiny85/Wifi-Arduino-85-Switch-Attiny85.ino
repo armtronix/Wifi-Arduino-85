ebc9fe08b52f5b04c0290fc16f6c0a651551b48e
armtronix <info@armtronix.net>
2016-04-17 11:02:23 +0530

    Wifi Switch code added for esp and attiny85

---
+++ Arduino Code/Wifi-Arduino-85-Switch-Attiny85/Wifi-Arduino-85-Switch-Attiny85.ino
@@ -0,0 +1,266 @@
+/*
+  Mqtt/http code  for Attiny85 microcontroller to send serial commands to esp8266-01 module 
+  for Wifi Arduino 85 board
+ Commands 
+ +ATturn_on , +ATon ---to turn on the relay 
+ +ATturn_off , +Atof ---to turn off the relay 
+ +ATstart           ---to enable sensor triggering relay
+ +ATstop            ---to disable sensor triggering relay
+
+ 
+ */
+#include <SoftwareSerial.h>
+int led = 0;
+int relay01 = 1;
+int pir = 2;
+//int PIR_STATUS_FLAG = 0;
+boolean ENABLE_SENSOR =0;
+boolean PREVIOUS_STATE =0;
+int pir_status;
+
+SoftwareSerial mySerial(3, 4); // RX, TX  Port 4 ---Rx Esp8266 Port 3---- Tx Esp8266
+
+void setup()  
+{
+  // Open serial communications and wait for port to open:
+  //Serial.begin(57600);
+  //while (!Serial) { wait for serial port to connect. Needed for Leonardo only
+ // }//
+
+
+ // Serial.println("Goodnight moon!");
+
+  // set the data rate for the SoftwareSerial port
+  pinMode(led, OUTPUT);
+  pinMode(relay01, OUTPUT);
+  pinMode(pir, INPUT);
+  mySerial.begin(4800);
+  delay(1000);
+  
+}
+
+
+
+void loop() // run over and over
+{
+  //if (mySerial.available())
+  //  Serial.write(mySerial.read());
+  //if (Serial.available())
+  //delay(20);
+  //mySerial.write(mySerial.read());
+   display_serial_loop();
+   // serialFlush(); 
+}
+//void serialFlush(){
+//  while(mySerial.available() > 0) {
+//    char t = mySerial.read();
+//  }
+//}  
+//unsigned long display_serial() {delay(100);}
+/*
+//unsigned long display_serial() 
+//{
+//char data[100];
+//char m;
+
+
+//while (mySerial.available() > 0)
+    {  
+      for(m=0; m<100; m++) 
+        {
+         if (mySerial.available() > 0) 
+          {
+           data[m]=mySerial.read();
+          }
+         else 
+          {
+           data[m]=0;
+          }
+        } 
+    }
+  
+    
+}
+*/
+
+unsigned long display_serial_loop() 
+{
+char data[45];
+ int m=0;
+ int n=45;
+ int o=0;
+ int x;
+ int y;
+ char ssid[24];
+ pir_status=digitalRead(pir);
+ 
+
+ 
+ 
+ 
+ for(m=0; m<n; m++) 
+        {
+        data[m]=0;     
+        }
+        
+   while (mySerial.available() > 0)
+       { 
+
+               if(m<n)
+                  {
+                  data[m]=mySerial.read();
+                  if(m>=2)
+                    {
+                    if(data[m-2]=='+')
+                     {
+                      if(data[m-1]=='A')
+                       {
+                         if(data[m]=='T')
+                         {
+                           o=m;
+                         }
+                         else
+                         {
+                         }
+                       }
+                       else
+                       {
+                       }
+                    }
+                    else
+                    {
+                    }
+                  
+                  }
+                  else
+                  {
+                  }
+                  m++;
+                  delay(1);
+                  }
+               else
+                 {
+                  m=0;
+                 }
+
+   
+       }
+                        
+     //for(m=0; m<n; m++) 
+       //{
+       if(data[o-2]=='+')
+        {
+                    if(data[o-1]=='A')
+                     {
+                       if(data[o]=='T')
+                       {
+                         //n=m;
+                         //b=data[o+7];
+                         //mySerial.println("AT+CIPSEND=0,2");
+                         //delay(2000);
+                         //mySerial.print(b);
+                         //mySerial.print("\n\r");
+                         //delay(200); //+IP,x,xx:turn_on //+APturn_on
+                                       //  o
+                          if(((data[o+1]=='t')&&(data[o+2]=='u')&&(data[o+6]=='o')&&(data[o+7]=='n'))||((data[o+1]=='o')&&(data[o+2]=='n')))
+                          {
+                              digitalWrite(relay01, HIGH);
+                              digitalWrite(led, LOW);
+                              //mySerial.println("AT+CIPSEND=0,12");
+                              //delay(70);
+                              mySerial.print("Relay on"); 
+                              mySerial.print("\n\r");
+
+                            }//+APturn_off
+                          else if(((data[o+1]=='t')&&(data[o+2]=='u')&&(data[o+6]=='o')&&(data[o+7]=='f')&&(data[o+8]=='f'))||((data[o+1]=='o')&&(data[o+2]=='f')))
+                            {
+                              digitalWrite(relay01, LOW);
+                              digitalWrite(led, HIGH);
+                              //mySerial.println("AT+CIPSEND=0,13");
+                              //delay(70);
+                              mySerial.print("Relay off");
+                              mySerial.print("\n\r"); 
+
+                            }
+                          else if(((data[o+1]=='s')&&(data[o+2]=='t')&&(data[o+3]=='a')&&(data[o+5]=='t')))
+                            {
+                               //+APstart
+                               //  o
+                              //digitalWrite(led, LOW);
+                              ENABLE_SENSOR=1;
+                             // mySerial.println("AT+CIPSEND=0,15");
+                             // delay(70);
+                              mySerial.print("Sensor Enb");
+                              mySerial.print("\n\r"); 
+
+                            }
+                         else if(((data[o+1]=='s')&&(data[o+2]=='t')&&(data[o+3]=='o')))
+                            {
+                              //+APstop
+                              //digitalWrite(led, HIGH);
+                              ENABLE_SENSOR=0;
+                             // mySerial.println("AT+CIPSEND=0,16");
+                             // delay(70);
+                              mySerial.print("Sensor Dis");
+                              mySerial.print("\n\r"); 
+
+                            }
+                          else if(((data[o+1]=='s')&&(data[o+2]=='t')&&(data[o+5]=='u')))
+                            { //+APstatus
+                              if(pir_status==1)
+                              {
+                                //digitalWrite(led, HIGH);
+                               // mySerial.println("AT+CIPSEND=0,20");
+                               // delay(70);
+                                mySerial.print("PIR HIGH");
+                                mySerial.print("\n\r"); 
+                              }
+                              else if(pir_status==0)
+                              {
+                                //digitalWrite(led, LOW);
+                                //mySerial.println("AT+CIPSEND=0,18");
+                                //delay(70);
+                                mySerial.print("PIR LOW");
+                                mySerial.print("\n\r");
+                              }
+                            }
+
+                         //break;
+                        }
+                       else
+                       {
+
+                       }
+                     }
+                     else
+                         {
+
+                         }
+         }
+       else
+         {
+
+         }
+delay(500);
+  if((ENABLE_SENSOR ==1) && (pir_status ==1)&& (PREVIOUS_STATE ==0))
+ {
+   mySerial.print("PIR HIGH");
+   mySerial.print("\n\r");
+   PREVIOUS_STATE=1;
+   digitalWrite(relay01, HIGH);
+   digitalWrite(led, LOW);
+ }
+ else if((ENABLE_SENSOR ==1) && (pir_status==0)&& (PREVIOUS_STATE==1))
+ {
+  mySerial.print("PIR LOW");
+  mySerial.print("\n\r");
+  PREVIOUS_STATE=0;
+   digitalWrite(relay01, LOW);
+   digitalWrite(led, HIGH);
+ }
+ else
+ {
+ }   
+     
+   }
+ 
---
+++ Arduino Code/Wifi-Arduino-85-Switch-Esp-01/mqttFunctions.ino
@@ -0,0 +1,103 @@
+boolean connectMQTT(){
+  if (mqttClient.connected()){
+    return true;
+  }  
+  
+  Serial.print("Connecting to MQTT server ");
+  Serial.print(mqttServer);
+  Serial.print(" as ");
+  Serial.println(host);
+  
+  if (mqttClient.connect(host)) {
+    Serial.println("Connected to MQTT broker");
+    if(mqttClient.subscribe((char*)subTopic.c_str())){
+      Serial.println("Subsribed to topic.");
+    } else {
+      Serial.println("NOT subsribed to topic!");      
+    }
+    return true;
+  }
+  else {
+    Serial.println("MQTT connect failed! ");
+    return false;
+  }
+}
+
+void disconnectMQTT(){
+  mqttClient.disconnect();
+}
+
+void mqtt_handler(){
+  if (toPub==1){
+    Debugln("DEBUG: Publishing state via MWTT");
+    if(pubState()){
+     toPub=0; 
+    }
+  }
+  mqttClient.loop();
+  delay(100); //let things happen in background
+}
+
+void mqtt_arrived(char* subTopic, byte* payload, unsigned int length) { // handle messages arrived 
+  int i = 0;
+  //Serial.print("MQTT message arrived:  topic: " + String(subTopic));
+    // create character buffer with ending null terminator (string)
+    
+  for(i=0; i<length; i++) {    
+    buf[i] = payload[i];
+  }
+  buf[i] = '\0';
+  String msgString = String(buf);
+  Serial.println(msgString);
+//  if (msgString == "R13_ON"){
+//      Serial.println("+ATturn_on");
+//      //Serial.println(digitalRead(OUTPIN));      
+//      //Serial.print("Switching light to "); 
+//      //Serial.println("high");
+//      //digitalWrite(OUTPIN, 1); 
+//  } else if (msgString == "R13_OFF"){
+//      Serial.println("+ATturn_off");
+      //Serial.println(digitalRead(OUTPIN));    
+      //Serial.print("Switching light to "); 
+     // Serial.println("low");
+     // digitalWrite(OUTPIN, 0); 
+//  }
+//  if (msgString == "Led_on"){
+//      Serial.print("Led is ");
+//      Serial.println(digitalRead(OUTLED));      
+//      Serial.print("Switching LED to "); 
+//      Serial.println("high");
+//      digitalWrite(OUTLED, 1); 
+//  } else if (msgString == "Led_off"){
+//      Serial.print("Led is ");
+//      Serial.println(digitalRead(OUTLED));    
+//      Serial.print("Switching LED to "); 
+//      Serial.println("low");
+//      digitalWrite(OUTLED, 0); 
+//  }        
+}
+
+boolean pubState(){ //Publish the current state of the light    
+  if (!connectMQTT()){
+      delay(100);
+      if (!connectMQTT){                            
+        Serial.println("Could not connect MQTT.");
+        Serial.println("Publish state NOK");
+        return false;
+      }
+    }
+    if (mqttClient.connected()){      
+        //String state = (digitalRead(OUTPIN))?"1":"0";
+        Serial.println("To publish state " + state );  
+      if (mqttClient.publish((char*)pubTopic.c_str(), (char*) state.c_str())) {
+        Serial.println("Publish state OK");        
+        return true;
+      } else {
+        Serial.println("Publish state NOK");        
+        return false;
+      }
+     } else {
+         Serial.println("Publish state NOK");
+         Serial.println("No MQTT connection.");        
+     }    
+}
